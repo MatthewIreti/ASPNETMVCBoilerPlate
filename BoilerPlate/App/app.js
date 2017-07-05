7124cbdfa5db38e292c8b567d65c4a3500d2c0d5
Matthew <dflobt@gmail.com>
2017-07-05 10:17:50 +0100

    removed cahce

--- BoilerPlate/App/app.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
-var app = angular.module("app", [
-    "ui.router",
-    "ui.bootstrap",
-    "ngSanitize",
-    "ui.router.stateHelper",
-    "ngResource"
-]);
-//# sourceMappingURL=app.js.map
--- BoilerPlate/App/models.js
+++
@@ -1,2 +0,0 @@
-;
-//# sourceMappingURL=models.js.map
